9c7728dfdb56f705eb8996aa24ee388758f72605
James Dawson <james.dawson@endjin.com>
2025-06-09 12:00:57 +0100

    Add support for generating an SBOM using the Covenant tool

---
+++ module/tasks/analysis.properties.ps1
@@ -0,0 +1,27 @@
+# <copyright file="analysis.properties.ps1" company="Endjin Limited">
+# Copyright (c) Endjin Limited. All rights reserved.
+# </copyright>
+
+# Synopsis: The version of the COvenant .NET global tool to install
+$covenantVersion = "0.20.0"
+
+# Synopsis: When true, an SPDX-formatted SBOM will be generated from the Covenant report 
+$CovenantIncludeSpdxReport = $true
+
+# Synopsis: When true, an CycloneDx-formatted SBOM will be generated from the Covenant report
+$CovenantIncludeCycloneDxReport = $true
+
+# Synopsis: A hashtable of additional metadata to be included in the Covenant report
+$CovenantMetadata = @{
+    git_repo = $(if (Get-Command "gh" -CommandType Application -ErrorAction Ignore) {
+        try {
+            $ghRepo = & gh repo view --json nameWithOwner
+            $ghRepo | ConvertFrom-Json | Select-Object -ExpandProperty nameWithOwner
+        }
+        catch {
+            ""
+        }
+    })
+    git_branch = ((Get-Command "git" -CommandType Application -ErrorAction Ignore) ? (& git branch --show-current) : "")
+    git_sha = ((Get-Command "git" -CommandType Application -ErrorAction Ignore) ? (& git rev-parse HEAD) : "")
+}
