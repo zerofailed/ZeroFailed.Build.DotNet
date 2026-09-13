346900aaa751588598aa6c4dcc2133f92ab7a871
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2026-09-13 16:46:30 +0100

    fix: update covenant version to 0.30.0 in analysis.properties.ps1

--- module/tasks/analysis.properties.ps1
+++ module/tasks/analysis.properties.ps1
@@ -3,7 +3,7 @@
 # </copyright>
 
 # Synopsis: The version of the Covenant .NET global tool to install
-$covenantVersion ??= "0.24.0"
+$covenantVersion ??= "0.30.0"
 
 # Synopsis: When true, an SPDX-formatted SBOM will be generated from the Covenant report 
 $CovenantIncludeSpdxReport ??= [Convert]::ToBoolean((property ZF_BUILD_DOTNET_COVENANT_INCLUDE_SPDX_REPORT $false))
